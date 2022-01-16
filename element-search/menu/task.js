77c14dd768d6ae4166ae218b2631448649a4d86d
luxeivan <luxeivan@gmail.com>
2022-01-16 20:56:16 +0300

    complete task2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,8 +1,19 @@
-let menu__items = document.getElementsByClassName("menu__item");
-console.log(menu__items);
-for (let i = 0; i < menu__items.length; i++) {
-    menu__items[i].querySelector('.menu__link').onclick = function(){
-        this.querySelector('.menu_sub').classList.add("menu_active");
-        return false;
+let menu__links = document.getElementsByClassName("menu__link");
+function closeAllSubmenu() {
+    let subMenus = document.getElementsByClassName("menu_sub");
+    for (let i = 0; i < subMenus.length; i++) {
+        subMenus[i].classList.remove("menu_active");
+    }
+};
+for (let i = 0; i < menu__links.length; i++) {
+    if (menu__links[i].parentElement.querySelector('.menu_sub')) {
+        menu__links[i].onclick = function () {
+            if (!this.parentElement.querySelector('.menu_sub').classList.contains("menu_active")) {
+                closeAllSubmenu();
+            }
+            this.parentElement.querySelector('.menu_sub').classList.toggle("menu_active");
+            return false;
+        };
     };
+
 }
